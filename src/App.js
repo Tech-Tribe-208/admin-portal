3a9f1cd90ecd2380b956a04fd0ea50ab46f340a7
favour <fackonu@st.ug.edu.gh>
2024-08-11 11:06:12 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          Admin Portal for Cleaning Services System
+        </p>
+    </div>
+  );
+}
+
+export default App;
