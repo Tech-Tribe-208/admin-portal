0beb83ac654553a002f21c4018f221527740da31
favour <fackonu@st.ug.edu.gh>
2024-08-19 00:21:42 +0000

    Get statistics from database

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import { DashboardContainer, StatsCard } from '../styles';
+import { useState } from 'react';
+import apiRequests from '../apiRequests';
 
 const DashboardPage = () => {
+  const [stats, setStats] = useState({
+    numberOfBookings: 0,
+    numberOfCleaners: 0,
+    numberOfCustomers: 0
+  });
+
+  useEffect(() => {
+    const fetchStats = async () => {
+      const response = await apiRequests.getStatistics();
+      if (response.status === 200) {
+        const { numberOfBookings, numberOfCleaners, numberOfCustomers } = response.data.responseData;
+        setStats({ numberOfBookings, numberOfCleaners, numberOfCustomers });
+      } else {
+        alert('Failed to fetch statistics');
+      }
+      console.log(response);
+    };
+    fetchStats();
+  }, []);
+
   return (
     <div>
       <Navbar />
       <DashboardContainer>
-        <StatsCard color="#D9CDAD">Number of Bookings: 10</StatsCard>
-        <StatsCard color="#1C422D">Number of Cleaners: 15</StatsCard>
-        <StatsCard color="#F7DB8D">Number of Customers: 20</StatsCard>
+        <StatsCard color="#D9CDAD">Number of Bookings: {stats.numberOfBookings}</StatsCard>
+        <StatsCard color="#1C422D">Number of Cleaners: {stats.numberOfCleaners}</StatsCard>
+        <StatsCard color="#F7DB8D">Number of Customers: {stats.numberOfCustomers}</StatsCard>
       </DashboardContainer>
     </div>
   );
